5009d3607c2ec91149c50a57b27f896313f5c819
ErikaNur <82448210+ErikaNur@users.noreply.github.com>
2022-03-14 16:28:09 +0700

    Add files via upload

---
+++ Versi Javascript-012-Erika Nur Amini/js/script.js
@@ -0,0 +1,15 @@
+/* Memunculkan Pop Up Button */
+alert("SELAMAT DATANG DI WEBSITE ERIKA NUR AMINI");
+
+/* Memanipulasi Teks */
+document.getElementById("gambar1").innerHTML = "Project One";
+
+document.getElementById("gambar2").innerHTML = "Project Two";
+
+document.getElementById("gambar3").innerHTML = "Project Three";
+
+document.getElementById("gambar4").innerHTML = "Project Four";
+
+document.getElementById("gambar5").innerHTML = "Project Five";
+
+document.getElementById("gambar6").innerHTML = "Project Six";
